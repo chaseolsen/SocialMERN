bb3aa219d67b47cd509f7a57124a9acba83680ed
chaseolsen <chase.olsen22@gmail.com>
2018-05-04 14:54:52 -0600

    Initial front end

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default () => {
+  return (
+    <div className="bg-dark text-white mt-5 p-4 text-center">
+      Copyright &copy; {new Date().getFullYear} DevConnector
+    </div>
+  )
+}

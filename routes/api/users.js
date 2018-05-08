e95008a837d133ae66debf5aa09300641a8e8dd5
chaseolsen <chase.olsen22@gmail.com>
2018-05-08 15:00:29 -0600

    User login authentication saving redux state and token

--- routes/api/users.js
+++ routes/api/users.js
@@ -30,7 +30,7 @@ router.get('/test', (req, res) => res.json({
 
 
 
-// @route    GET api/users/register
+// @route    POST api/users/register
 // @desc     Register user
 // @access   Public
 router.post('/register', (req, res) => {
